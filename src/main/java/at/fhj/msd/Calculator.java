f2e6076746c5c9c1d99e741546ccdfcd9d4fd5ce
Franz Lafer <franz.lafer@edu.fh-joanneum.at>
2022-04-06 15:23:03 +0200

    6.4.2022

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,2 +1,20 @@
-package at.fhj.msd;public class Calculator {
+package at.fhj.msd;
+
+public class Calculator {
+
+    public double add (double number1, double number2){
+        return number1 + number2;
+    }
+
+    public double minus (double number1, double number2){
+        return number1 - number2;
+    }
+
+    public double multiply (double number1, double number2){
+        return number1 - number2;
+    }
+
+    public double divide (double number1, double number2){
+        return number1/number2;
+    }
 }
--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,2 +1,12 @@
-package at.fhj.msd;public class Main {
+package at.fhj.msd;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        System.out.println(c.add(2,3));
+        System.out.println(c.minus(5,10));
+        System.out.println(c.divide(3,5));
+        System.out.println(c.multiply(7,4));
+    }
 }
